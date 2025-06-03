45891e8ba1edea3631426004310c478c8a6ea845
Nikola55555 <8637868@mail.ru>
2025-06-03 20:42:56 +0300

    Connected Allure

--- tests/LoginTests.py
+++ tests/LoginTests.py
@@ -1,17 +1,26 @@
 from core.BaseTest import browser
 from pages.BasePage import BasePage
 from pages.LoginPage import LoginPageHelper
+import allure
+
 
 BASE_URL = 'https://ok.ru/'
 EMPTY_LOGIN_ERROR = 'Введите логин'
 EMPTY_PASSWORD_ERROR = 'Введите пароль'
 
+@allure.suite('Проверка фомы авторизации')
+@allure.title('Проверка ошибки при пустой форме авторизации')
+@allure.description("При отправке пустой формы система должна показать ошибку 'Введите логин'")
 def test_empty_login_and_password(browser):
     BasePage(browser).get_url(BASE_URL)
     LoginPage = LoginPageHelper(browser)
     LoginPage.click_login()
     assert LoginPage.get_error_text() == EMPTY_LOGIN_ERROR
 
+
+@allure.suite('Проверка фомы авторизации')
+@allure.title('Проверка ошибки при введенном логине и пустом пароле')
+@allure.description("При отправке формы с заполненным логином, но пустым паролем система должна показать ошибку 'Введите пароль'")
 def test_valid_login_and_empty_password(browser):
     BasePage(browser).get_url(BASE_URL)
     LoginPage = LoginPageHelper(browser)
