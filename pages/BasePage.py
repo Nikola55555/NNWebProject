ed44ec48effda5455f0f5b260db7d77bc113736b
Nikola55555 <8637868@mail.ru>
2025-06-04 21:09:01 +0300

    Добавлен тест: test_registration_random_country

--- pages/BasePage.py
+++ pages/BasePage.py
@@ -10,6 +10,10 @@ class BasePage:
     def find_element(self, locator, time=5):
         return WebDriverWait(self.driver, time).until(EC.visibility_of_element_located(locator),
                                                       message=f"Не удалось найти элемент {locator}")
+    def find_elements(self, locator, time=5):
+        return WebDriverWait(self.driver, time).until(EC.visibility_of_all_elements_located(locator),
+                                                      message=f"Не удалось найти элементы {locator}")
+
 
     @allure.step(f'Открываем страницу')
     def get_url(self, url):
