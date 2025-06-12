16964f3a4ac3a95fdac762c675edc3d3a5173a2e
Nick <94436320+Nikola55555@users.noreply.github.com>
2025-06-12 18:16:11 +0300

    Update BaseTest.py

--- core/BaseTest.py
+++ core/BaseTest.py
@@ -2,12 +2,12 @@ import pytest
 from selenium import webdriver
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope='function')
 def browser():
     options = webdriver.ChromeOptions()
     options.add_argument('--lang=ru')
-    #driver = webdriver.Remote(command_executor='http://31.207.74.100:4444/wd/hub', options=options)
-    driver = webdriver.Remote(command_executor='http://31.207.74.100:4444', options=options)
+    driver = webdriver.Remote(command_executor='http://31.207.74.100:4444/wd/hub', options=options)
+    #driver = webdriver.Remote(command_executor='http://31.207.74.100:4444', options=options)
     yield driver
     if driver:
         driver.quit()
